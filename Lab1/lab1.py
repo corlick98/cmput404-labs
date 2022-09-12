f7f42ac31d5a963659b6f609d101919293a5086d
Christopher Orlick <corlick@ualberta.ca>
2022-09-12 17:36:28 -0600

    prints own source

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,3 +1,4 @@
 import requests
 
-print(requests.get('http://google.com'))
+my_script = requests.get('https://raw.githubusercontent.com/corlick98/cmput404-labs/master/Lab1/lab1.py')
+print(my_script.content)
